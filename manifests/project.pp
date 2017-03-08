0495699c50bc6d62e74806943cc6215298b55730
Jordan Conway <jconway@linuxfoundation.org>
2017-03-08 12:06:13 -0500

    Ensure projects are latest
    Signed-off-by: Jordan Conway <jconway@linuxfoundation.org>

--- manifests/project.pp
+++ manifests/project.pp
@@ -1,7 +1,7 @@
 # == Define opengrok::project
 
 define opengrok::project (
-  Enum['present', 'absent', 'latest'] $ensure = present,
+  Enum['present', 'absent', 'latest'] $ensure = latest,
   String $provider = 'git',
   String $source = undef,
   Optional[String[1]] $revision = undef,

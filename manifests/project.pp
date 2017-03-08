857cc91a11dc8f93ff2e459bc1accfcb6c18973f
Jordan Conway <jconway@linuxfoundation.org>
2017-03-08 17:22:21 -0500

    Document opengrok::project type
    Signed-off-by: Jordan Conway <jconway@linuxfoundation.org>

--- manifests/project.pp
+++ manifests/project.pp
@@ -1,4 +1,21 @@
-# == Define opengrok::project
+# Define opengrok::project
+# 
+# This define lets you add git projects to OpenGrok
+#
+# @example Defining a git project for OpenGrok
+#   opengrok::project { 'puppet-opengrok:
+#     ensure => 'latest',
+#     source => 'https://github.com/jordanconway/puppet-opengrok.git',
+#   }
+#
+# @param ensure Ensure the project is included, latest or absent. Valid Options:
+#   strings 'present', 'absent', 'latest'. Example Value: 'latest'
+# @param provider The vcs provider for the project. CURRENTLY ONLY SUPPORTS 'git'
+#   Valid Options: String. Example Value: 'git'
+# @param source A link to the git repo being added. Valid Options: String
+#   Example Value: 'https://github.com/jordanconway/puppet-opengrok.git'
+# @param revision A git revision reference to pin the repo to. Valid Options: String
+#   Example Value: '9db36f3c12cb57bde8c2cdf4b66bf1745bea9968'
 
 define opengrok::project (
   Enum['present', 'absent', 'latest'] $ensure = latest,

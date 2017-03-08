b330d0bc7ae244ff9a736df174d0a1ebc700e611
Jordan Conway <jconway@linuxfoundation.org>
2017-03-08 17:00:19 -0500

    puppet-strings document get_opengrok_fname
    Signed-off-by: Jordan Conway <jconway@linuxfoundation.org>

--- lib/puppet/functions/get_opengrok_fname.rb
+++ lib/puppet/functions/get_opengrok_fname.rb
@@ -1,4 +1,9 @@
+# get_opengrok_fname function
 Puppet::Functions.create_function(:get_opengrok_fname) do
+  # @param opengrok_url Takes a full download url from https://github.com/OpenGrok/OpenGrok/releases
+  # @return [String] Returns a string of just the filename of the download.
+  # @example Calling the function
+  #   get_opengrok_fname('https://github.com/OpenGrok/OpenGrok/files/467358/opengrok-0.12.1.6.tar.gz.zip')
   dispatch :get_opengrok_fname do
     param 'String', :opengrok_url
   end
